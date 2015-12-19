164b7dd5e29dac375dc7d35893b70f9b78b781f8
Sven Gregori <svengregori@gmail.com>
2015-12-19 02:43:32 +0200

    Added GPL text

--- firmware/lcd3310.c
+++ firmware/lcd3310.c
@@ -3,6 +3,18 @@
  *
  * Copyright (C) 2015 Sven Gregori <svengregori@gmail.com>
  *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/
+ *
  *
  * The Nokia LCD3310 LCD is a 48x84px display that is arranged in 6 rows of
  * 8x84px each. Each row is therefore 84 byte wide with each byte representing
