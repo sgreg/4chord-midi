fed9f523dd241252ee00976b4ef5dc4788881e10
Sven Gregori <svengregori@gmail.com>
2017-08-10 02:21:15 +0300

    issue-#14: Changed USB VID/PID to 4chord MIDI's own
    Cheers to pid.codes for making this possible.

--- firmware/usbconfig.h
+++ firmware/usbconfig.h
@@ -210,7 +210,7 @@
 
 /* -------------------------- Device Description --------------------------- */
 
-#define  USB_CFG_VENDOR_ID       0xc0, 0x16 /* = 0x16c0 = 5824 = voti.nl */
+#define  USB_CFG_VENDOR_ID      0x09, 0x12 /* = 0x1209 = pid.codes */
 /* USB vendor ID for the device, low byte first. If you have registered your
  * own Vendor ID, define it here. Otherwise you may use one of obdev's free
  * shared VID/PID pairs. Be sure to read USB-IDs-for-free.txt for rules!
@@ -219,7 +219,7 @@
  * with libusb: 0x16c0/0x5dc.  Use this VID/PID pair ONLY if you understand
  * the implications!
  */
-#define  USB_CFG_DEVICE_ID       0xe4, 0x05 /* = 0x05e4 = 1508, obdev MIDI */
+#define  USB_CFG_DEVICE_ID      0xaf, 0xde /* = 0xdeaf = CrapLab 4chord MIDI */
 /* This is the ID of the product, low byte first. It is interpreted in the
  * scope of the vendor ID. If you have registered your own VID with usb.org
  * or if you have licensed a PID from somebody else, define it here. Otherwise
