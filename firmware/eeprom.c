bd083b65af02ea9cd9df6f47bbbc02578e610604
Sven Gregori <svengregori@gmail.com>
2018-05-07 23:32:33 +0300

    issue-#25: Initial EEPROM data handling added
    Dummy EEPROM data defined and files are added to Makefile

---
+++ firmware/eeprom.c
@@ -0,0 +1,24 @@
+/*
+ * 4chord MIDI - EEPROM data
+ *
+ * Copyright (C) 2018 Sven Gregori <sven@craplab.fi>
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/
+ *
+ */
+#include <avr/eeprom.h>
+#include "eeprom.h"
+
+struct eeprom_data_t eeprom_data EEMEM = {
+    .dummy = "something",
+};
